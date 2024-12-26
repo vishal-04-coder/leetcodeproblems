d6d11fe5aa93884b6c629b16a9fd8f3e2dbf191f
vishal-04-coder <vishal.a0410@gmail.com>
2024-12-26 15:06:24 +0530

    [155] [Time Beats: 70.03%] [Memory Beats: 5.77%] - LeetPush

---
+++ 155-Min-Stack.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+public:
+    MinStack() {
+        s2.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        s1.push(val);
+        s2.push(min(val,s2.top()));
+    }
+    
+    void pop() {
+        s1.pop();
+        s2.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+    private:
+    stack<int>s1;
+    stack <int>s2;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
