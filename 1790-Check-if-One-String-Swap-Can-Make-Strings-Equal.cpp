f2da1f778482600ea7edad18ac98df9e5f9b747f
Vishal A <vishal.a0410@gmail.com>
2025-02-05 11:32:21 +0530

    [1790] [Time Beats: 100.00%] [Memory Beats: 36.33%] - LeetPush

---
+++ 1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.size();
+        int c = -1;
+        for (int i = 0; i < n; i++) {
+            if (s1[i] != s2[i]) {
+                if (c == -1) {
+                    c = i;
+                }
+
+                else {
+                    swap(s1[i], s1[c]);
+                    return s1 == s2;
+                }
+            }
+        }
+
+        return s1 == s2;
+    }
+};
