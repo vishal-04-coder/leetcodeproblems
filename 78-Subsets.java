59852a6dc2f3392a634f0c4a8bfedd4fe2a87fd1
Vishal A <vishal.a0410@gmail.com>
2025-02-15 13:22:36 +0530

    [78] [Time Beats: 100.00%] [Memory Beats: 30.72%] - LeetPush

---
+++ 78-Subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>res=new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<(1<<n);i++)
+        {
+            List<Integer>list=new ArrayList<>();
+
+            for(int j=0;j<n;j++){
+                if( (i & (1<<j))>0){
+                    list.add(nums[j]);
+                }
+                
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
