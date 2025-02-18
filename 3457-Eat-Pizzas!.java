9e222d2303850d5e78bacb7169775eaa1eb889fb
Vishal A <vishal.a0410@gmail.com>
2025-02-18 11:04:21 +0530

    [3457] [Time Beats: 40.14%] [Memory Beats: 36.94%] - LeetPush

---
+++ 3457-Eat-Pizzas!.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long maxWeight(int[] pizzas) {
+        Arrays.sort(pizzas);
+        int n = pizzas.length, days = n / 4;
+        long totalWeight = 0;
+        int index = n - 1;
+
+        for (int day = 1; day <= days; day += 2) {
+            totalWeight += pizzas[index--];
+        }
+
+        index--;
+        for (int day = 2; day <= days; day += 2) {
+            totalWeight += pizzas[index];
+            index -= 2;
+        }
+        return totalWeight;
+    }
+}
