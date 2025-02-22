b4311dfcfbd75fea121b10b705e2b105e172668d
Vishal A <vishal.a0410@gmail.com>
2025-02-22 14:06:40 +0530

    [350] [Time Beats: 89.75%] [Memory Beats: 64.37%] - LeetPush

---
+++ 350-Intersection-of-Two-Arrays-II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        List<Integer>list=new ArrayList<>();
+        for(int i:nums1){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:nums2){
+            if(map.containsKey(i) && map.get(i)>0){
+                list.add(i);
+                 map.put(i,map.get(i)-1);
+            }
+        }
+        int []arr=new int[list.size()];
+         for(int i=0;i<list.size();i++){
+            arr[i]=list.get(i);
+         }
+        return arr;
+    }
+}
